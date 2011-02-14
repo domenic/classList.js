6bc35ec513b94de533399f111bd9323d7d3f085f
DomenicDenicola <domenic@domenicdenicola.com>
2011-02-14 08:49:23 -0800

    Reformatting, redoing the way in which properties are removed, and fixing some dependencies on Array properties that were removed. (E.g., now we use Array.prototype.push.call(this, x) instead of this.push(x).)

--- classList.js
+++ classList.js
@@ -7,137 +7,118 @@
  * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
  */
 
-/*jslint laxbreak: true, eqeqeq: true, newcap: true, immed: true, strict: true,
-  maxlen: 90 */
-/*global Element */
+/*jslint browser: true, eqeqeq: true, newcap: true, undef: true, strict: true, white: true */
+/*global DOMException */
 
 /*! @source https://github.com/DomenicDenicola/classList.js/file-edit/master/classList.js */
 
 /* forked and edited by Domenic Denicola: see @source link for changelog. */
 
 if (!("classList" in document.createElement("div"))) {
+	(function () {
+		//#region DOM Exception
+		// Vendors: please allow content code to instantiate DOMExceptions
+		function DOMEx(type, message) {
+			this.name = type;
+			this.code = DOMException[type];
+			this.message = message;
+		}
+		// Most DOMException implementations don't allow calling DOMException's toString()
+		// on non-DOMExceptions. Error's toString() is sufficient here.
+		DOMEx.prototype = Error.prototype;
+		//#endregion
 
-(function () {
-
-var
-	  classListProp = "classList"
-	, protoProp = "prototype"
-	, elemCtrProto = Element[protoProp]
-	, objCtr = Object
-	, strTrim = String[protoProp].trim || function () {
-		return this.replace(/^\s+|\s+$/g, "");
-	}
-	, arrIndexOf = Array[protoProp].indexOf || function (item) {
-		for (var i = 0, len = this.length; i < len; i++) {
-			if (i in this && this[i] === item) {
-				return i;
+		//#region ClassList helper function, constructor, and prototype modifications.
+		function checkTokenAndGetIndex(classList, token) {
+			if (token === "") {
+				throw new DOMEx("SYNTAX_ERR", "An invalid or illegal string was specified");
 			}
+			if (/\s/.test(token)) {
+				throw new DOMEx("INVALID_CHARACTER_ERR", "String contains an invalid character");
+			}
+			return Array.prototype.indexOf.call(classList, token);
 		}
-		return -1;
-	}
-	// Vendors: please allow content code to instantiate DOMExceptions
-	, DOMEx = function (type, message) {
-		this.name = type;
-		this.code = DOMException[type];
-		this.message = message;
-	}
-	, checkTokenAndGetIndex = function (classList, token) {
-		if (token === "") {
-			throw new DOMEx(
-				  "SYNTAX_ERR"
-				, "An invalid or illegal string was specified"
-			);
+
+		function ClassList(elem) {
+			var trimmedClasses = elem.className.trim();
+			var classes = trimmedClasses ? trimmedClasses.split(/\s+/) : [];
+
+			for (var i = 0, len = classes.length; i < len; i++) {
+				Array.prototype.push.call(this, classes[i]);
+			}
+
+			this._updateClassName = function () {
+				elem.className = this.toString();
+			};
 		}
-		if (/\s/.test(token)) {
-			throw new DOMEx(
-				  "INVALID_CHARACTER_ERR"
-				, "String contains an invalid character"
-			);
-		}
-		return arrIndexOf.call(classList, token);
-	}
-	, ClassList = function (elem) {
-		var
-			  trimmedClasses = strTrim.call(elem.className)
-			, classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
-		;
-		for (var i = 0, len = classes.length; i < len; i++) {
-			this.push(classes[i]);
-		}
-		this._updateClassName = function () {
-			elem.className = this.toString();
+		ClassList.prototype = [];
+
+		ClassList.prototype.item = function (i) {
+			return this[i] || null;
 		};
-	}
-	, classListProto = ClassList[protoProp] = []
-	, classListGetter = function () {
-		var classList = new ClassList(this);
+		ClassList.prototype.contains = function (token) {
+			token += "";
+			return checkTokenAndGetIndex(this, token) !== -1;
+		};
+		ClassList.prototype.add = function (token) {
+			token += "";
+			if (checkTokenAndGetIndex(this, token) === -1) {
+				Array.prototype.push.call(this, token);
+				this._updateClassName();
+			}
+		};
+		ClassList.prototype.remove = function (token) {
+			token += "";
+			var index = checkTokenAndGetIndex(this, token);
+			if (index !== -1) {
+				Array.prototype.splice.call(this, index, 1);
+				this._updateClassName();
+			}
+		};
+		ClassList.prototype.toggle = function (token) {
+			token += "";
+			if (checkTokenAndGetIndex(this, token) === -1) {
+				this.add(token);
+			} else {
+				this.remove(token);
+			}
+		};
+		ClassList.prototype.toString = function () {
+			return Array.prototype.join.call(this, " ");
+		};
 
-		// Remove all properties we've prototyped onto Array ourselves, since they will have enumerable: true.
+		// "Remove" (i.e. overwrite with undefined) all properties we've prototyped onto Array ourselves, since they will have enumerable: true.
 		for (var propName in Array.prototype) {
-			classList[propName] = undefined;
+			ClassList.prototype[propName] = undefined;
+		}
+		// "Remove" (i.e. overwrite with undefined) all built-in array properties.
+		var builtInArrayProperties = ["concat", "every", "filter", "forEach", "indexOf", "join", "lastIndexOf", "map", "pop", "push", "reduce", "reduceRight", "reverse", "shift", "slice", "some", "sort", "splice", "toLocaleString", "unshift"];
+		for (var i = 0, builtInPropName; builtInPropName = builtInArrayProperties[i]; ++i) {
+			ClassList.prototype[builtInPropName] = undefined;
+		}
+		//#endregion
+
+		//#region Adding the classList property to all Element objects.
+		var CLASS_LIST_PROPERTY_NAME = "classList";
+		function classListGetter() {
+			return new ClassList(this);
 		}
 
-		// Remove all built-in array properties.
-		classList.concat = classList.every = classList.filter = classList.forEach = classList.indexOf = classList.join = classList.lastIndexOf = classList.map = classList.pop = classList.push = classList.reduce = classList.reduceRight = classList.reverse = classList.shift = classList.slice = classList.some = classList.sort = classList.splice = classList.toLocaleString = classList.unshift = undefined;
-
-		return classList;
-	}
-;
-// Most DOMException implementations don't allow calling DOMException's toString()
-// on non-DOMExceptions. Error's toString() is sufficient here.
-DOMEx[protoProp] = Error[protoProp];
-classListProto.item = function (i) {
-	return this[i] || null;
-};
-classListProto.contains = function (token) {
-	token += "";
-	return checkTokenAndGetIndex(this, token) !== -1;
-};
-classListProto.add = function (token) {
-	token += "";
-	if (checkTokenAndGetIndex(this, token) === -1) {
-		this.push(token);
-		this._updateClassName();
-	}
-};
-classListProto.remove = function (token) {
-	token += "";
-	var index = checkTokenAndGetIndex(this, token);
-	if (index !== -1) {
-		this.splice(index, 1);
-		this._updateClassName();
-	}
-};
-classListProto.toggle = function (token) {
-	token += "";
-	if (checkTokenAndGetIndex(this, token) === -1) {
-		this.add(token);
-	} else {
-		this.remove(token);
-	}
-};
-classListProto.toString = function () {
-	return this.join(" ");
-};
-
-if (objCtr.defineProperty) {
-	var classListDescriptor = {
-		  get: classListGetter
-		, enumerable: true
-		, configurable: true
-	};
-	try {
-		objCtr.defineProperty(elemCtrProto, classListProp, classListDescriptor);
-	} catch (ex) { // IE 8 doesn't support enumerable:true
-		if (ex.number === -0x7FF5EC54) {
-			classListDescriptor.enumerable = false;
-			objCtr.defineProperty(elemCtrProto, classListProp, classListDescriptor);
+		if (Object.defineProperty) {
+			var classListDescriptor = { get: classListGetter, enumerable: true, configurable: true };
+			try {
+				Object.defineProperty(Element.prototype, CLASS_LIST_PROPERTY_NAME, classListDescriptor);
+			} catch (ex) { // IE 8 doesn't support enumerable: true
+				if (ex.number === -0x7FF5EC54) {
+					classListDescriptor.enumerable = false;
+					Object.defineProperty(Element.prototype, CLASS_LIST_PROPERTY_NAME, classListDescriptor);
+				} else {
+					throw ex;
+				}
+			}
+		} else if (Object.prototype.__defineGetter__) {
+			Element.prototype.__defineGetter__(CLASS_LIST_PROPERTY_NAME, classListGetter);
 		}
-	}
-} else if (objCtr[protoProp].__defineGetter__) {
-	elemCtrProto.__defineGetter__(classListProp, classListGetter);
-}
-
-}());
-
+		//#endregion
+	}());
 }
